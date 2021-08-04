72aee77d2db22a8645c72bd677586346e9dc2c4a
yassir78 <yassir.acaf@gmail.com>
2021-08-04 20:09:51 +0100

    home 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterModule } from './modules/register/register.module';
-
+import { AnimateOnScrollModule } from 'ng2-animate-on-scroll';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,6 +13,8 @@ import { RegisterModule } from './modules/register/register.module';
     BrowserModule,
     RegisterModule,
     AppRoutingModule, 
+    AnimateOnScrollModule.forRoot()
+
   ],
 
   providers: [],
