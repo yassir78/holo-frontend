9663a16b4f5b8fa56b6897f9b9493ae79dc8408e
yassir78 <yassir.acaf@gmail.com>
2021-08-28 15:47:32 +0100

    changes

--- src/app/models/property.ts
+++ src/app/models/property.ts
@@ -1,15 +1,15 @@
-export interface Property{
-  
-   currentDonor?:string;
-   numberOfPieces?:number;
-   currentRent?:number;
-   leaseInYourName?:string;
-   ReasonForChange?:string;
-   howDidYouFindThisObject?:string;
-   doYouHaveACar?:string;
-   HaveACarHowMuch?:number;
-   areYouInterestedInAParkingSpace?:string;
-   InterestedInAParkingSpaceHowMuch?:number,
-   refrences?:string;
-   possibleRemarks?:string;
+export interface Property {
+
+   currentDonor?: string;
+   numberOfPieces?: number;
+   currentRent?: number;
+   leaseInYourName?: string;
+   ReasonForChange?: string;
+   howDidYouFindThisObject?: string;
+   doYouHaveACar?: string;
+   HaveACarHowMuch?: number;
+   areYouInterestedInAParkingSpace?: string;
+   InterestedInAParkingSpaceHowMuch?: number,
+   refrences?: string;
+   possibleRemarks?: string;
 }
