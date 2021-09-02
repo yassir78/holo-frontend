8cdab9118133fd43cc9ecbf3ecabe67750e8ecc2
yassir78 <yassir.acaf@gmail.com>
2021-09-02 13:51:13 +0100

    dashboard sidenav implementation

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidenavComponent
   ]
 })
 export class SharedModule { }
