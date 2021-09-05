23a08bf05cb41bd403f61a0e93e75e6818376020
yassir78 <yassir.acaf@gmail.com>
2021-09-05 13:13:13 +0100

    responsive add class on active route

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,45 +3,46 @@ const { guessProductionMode } = require("@ngneat/tailwind");
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 const colors = require('tailwindcss/colors')
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
-    },  
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-      screens: {
-        'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-  
-        'md': '768px',
-        // => @media (min-width: 768px) { ... }
-  
-        'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
-      },
-      extend: {
-          colors: {
-          'warm-gray': colors.warmGray,
-          teal: colors.teal,
-        }
-      },
+  prefix: '',
+  mode: 'jit',
+  purge: {
+    content: [
+      './src/**/*.{html,ts,css,scss,sass,less,styl}',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'fold': '280',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
     },
-    variants: {
-      extend: {},
+    extend: {
+      colors: {
+        'warm-gray': colors.warmGray,
+        teal: colors.teal,
+      }
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-      require('@tailwindcss/aspect-ratio'),
-    ],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
   prefix: '',
   mode: 'jit',
   purge: {
@@ -59,7 +60,7 @@ module.exports = {
     },
   },
   variants: {
-    extend: { },
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
