7ce5cd3c418e0ac5643e282fb2c51801a589d774
yassir78 <yassir.acaf@gmail.com>
2021-09-11 16:19:30 +0100

    good add implementation : input validation

--- src/app/modules/dashboard/bailleur/add-good/information/information.component.ts
+++ src/app/modules/dashboard/bailleur/add-good/information/information.component.ts
@@ -1,5 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'information',
@@ -14,9 +16,49 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class InformationComponent implements OnInit {
-  selectDropDown: boolean = false;
-  constructor() { }
+  venteOuLocation: string = 'Vente';
+  selectRegieDropDown: boolean = false;
+  selectRegionDropDown: boolean = false;
+  biens: String[] = ['Appartements', 'Meublé', 'Villas / Maison', 'Commerciaux', 'Terrain', 'Immeuble', 'Logt vacances', 'Parkings']
+  selectedRegieValue: string = 'Choisissez une régie';
+  document: string = "+ Ajouter";
+  progress$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
+  informationForm: FormGroup = new FormGroup({
+    saleOrRental: new FormControl('Vente', Validators.required),
+    disponibilityDate: new FormControl('', Validators.required),
+    management: new FormControl('', Validators.required),
+    typeOfGood: new FormArray([], Validators.required),
+    zipCode: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+    livingArea: new FormControl('', Validators.required),
+    terraceBalconyArea: new FormControl('', Validators.required),
+    gardenArea: new FormControl('', Validators.required)
+  })
+  constructor() { }
+  documentUpload(event: any) {
+    console.log(event)
+    this.document = event.target.files[0].name;
+    // upload process
+  }
+  typeDeBienValue(value: String, event: any) {
+    const typeDeBienArray = <FormArray>this.informationForm.controls.typeOfGood;
+    const checked: boolean = event.target.checked;
+    if (checked) {
+      typeDeBienArray.push(new FormControl(value))
+    } else {
+      const index = typeDeBienArray.controls.findIndex(val => val.value == value);
+      index > -1 ? typeDeBienArray.removeAt(index) : false;
+    }
+    console.log(typeDeBienArray.controls)
+
+  }
+  managementDropDown(value: string) {
+    this.informationForm.get('management')?.setValue(value);
+    this.selectedRegieValue = value;
+    this.selectRegieDropDown = false;
+    console.log(this.informationForm.get('management')?.value)
+  }
   ngOnInit(): void {
   }
 
