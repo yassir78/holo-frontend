2974704e41cc51ce65631858de827565ae1c461a
yassir78 <yassir.acaf@gmail.com>
2021-09-14 18:34:03 +0100

    add good media upload implementation fix

--- src/app/modules/dashboard/bailleur/add-good/description/description.component.ts
+++ src/app/modules/dashboard/bailleur/add-good/description/description.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ChangeEvent } from '@ckeditor/ckeditor5-angular';
@@ -11,7 +11,7 @@ import { bailleurDashboardState } from '../../state/bailleurdb.state';
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.scss']
 })
-export class DescriptionComponent implements OnInit {
+export class DescriptionComponent implements OnInit, OnDestroy {
   public Editor = ClassicEditor;
   descriptionBien: string = "<p style='color:red'>Description de votre bien . . .</p>";
   descForm: FormGroup = new FormGroup({
@@ -19,18 +19,21 @@ export class DescriptionComponent implements OnInit {
     description: new FormControl('', Validators.required)
 
   })
-  constructor(private store: Store<bailleurDashboardState>, private router:Router) {
+  constructor(private store: Store<bailleurDashboardState>, private router: Router) {
 
+  }
+  ngOnDestroy(): void {
+    console.log("this is the destroy method")
   }
   ngOnInit(): void {
   }
 
-  get propertyName(){
-   return this.descForm.get("propertyName");
+  get propertyName() {
+    return this.descForm.get("propertyName");
   }
-  get description(){
+  get description() {
     return this.descForm.get("description");
-   }
+  }
   public onChangeText({ editor }: ChangeEvent) {
     const data = editor.getData();
     console.log(data)
@@ -38,20 +41,16 @@ export class DescriptionComponent implements OnInit {
     this.description?.setValue(this.descriptionBien);
   }
 
-  formSubmit(){
+  formSubmit() {
     console.log({
       propertyName: this.propertyName?.value,
       description: this.descriptionBien
     })
-
     this.store.dispatch(BailleurdbActions.description({
       propertyName: this.propertyName?.value,
       description: this.descriptionBien
     }))
-
     this.router.navigate(['/dashboard/bailleur/add-good/media'])
-
-  
   }
 
 }
