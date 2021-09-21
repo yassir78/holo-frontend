f867590545771449b021b32d6d5901c8a166b7f3
yassir78 <yassir.acaf@gmail.com>
2021-09-21 09:31:35 +0100

    changes

--- src/app/models/datesAndHours.ts
+++ src/app/models/datesAndHours.ts
@@ -1,6 +1,5 @@
-import * as moment from 'moment';
-export interface DateAndHours { 
-    date?: string, 
-    startHour?: string, 
+export interface DateAndHours {
+    date?: string,
+    startHour?: string,
     finishHour?: string
 }
