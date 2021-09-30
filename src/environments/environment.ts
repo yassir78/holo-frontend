7eb82e32012a808347caac7aad111640fba9aa91
yassir78 <yassir.acaf@gmail.com>
2021-09-30 11:25:37 +0100

    prod base_url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  base_url: "http://localhost:3000",
+  base_url: "http://ec2-3-84-236-243.compute-1.amazonaws.com:3000/",
   campainId: "5kw03",
-  customFieldId:"p8XrIo",
-  getResponseApiKey:"8ndtifh1asicn585irsfw55hyuliryai",
+  customFieldId: "p8XrIo",
+  getResponseApiKey: "8ndtifh1asicn585irsfw55hyuliryai",
   awsAccesskeyID: "AKIA2EE4YWSIR7KVQBEN",
   awsSecretAccessKey: "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",
   awsRegion: "eu-west-3",
